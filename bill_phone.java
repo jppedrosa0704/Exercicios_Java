ea70cf69dbd700ee04ee86d5cc4c3aca73d97903
Hi there! I'm João Paulo <jp-pedrosa@hotmail.com>
2026-02-11 16:00:32 +0000

    Bill phone
    SUMMARY OF THE EXERCISE
    
    This program simulates a simple phone bill calculator:
    
    Base price: R$50 for up to 100 minutes
    
    Extra minutes cost: R$2 per minute
    
    The user types how many minutes were used
    
    The program calculates and prints the final amount to pay

---
+++ bill_phone.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class bill_phone{
+
+    public static void main(String[] args) {
+
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Os minutos usados foram:");
+    
+    int minutos = sc.nextInt();
+
+    double conta = 50.0;
+    if (minutos > 100){
+        conta = conta + (minutos - 100) * 2.0;
+    }
+
+    System.out.printf("Valor a pagar e = R$ %.2f%n", conta);
+
+    sc.close();
+}
+
+}
